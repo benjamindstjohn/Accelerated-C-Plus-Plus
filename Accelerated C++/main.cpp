915d00817be408c6e89510030da8ad7508294bf4
Benjamin (D.) St John <131232324+sanctusjack@users.noreply.github.com>
2023-11-15 07:46:29 -0600

    Add files via upload

---
+++ Accelerated C++/main.cpp
@@ -0,0 +1,16 @@
+// A small C++ Program.
+
+// You can write comments in C++ by using two backslashes.
+
+// C++ communicates with standard-library facilities by using #include directives.
+
+// Since the directive is enclosed in (< and >), we know it refers to a part of the C++ library.
+
+// called the standard header.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
